6c985c2f95d6818eed4068f4f52cc0990ae8b367
Sumit Dhimal <sumit.dhimal.333@gmail.com>
2026-01-21 21:13:06 +0545

    Tranfering to Linux

--- client/src/pages/blog/CreateBlog.jsx
+++ client/src/pages/blog/CreateBlog.jsx
@@ -10,32 +10,37 @@ const CreateBlog = () => {
     }
 
   return (
-    <div className='max-w-225 mx-auto mt-12'> 
-        <input 
-            type='text'
-            name='title'
-            placeholder='Title of your story...'
-            required
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            className='border-0 rounded-lg bg-gray-100 focus:outline-none w-full text-3xl font-serif px-5 py-3 mb-6'
-        />
+    <div className='max-w-225 mx-auto mt-12'>
+        <div className="mb-5 flex flex-row-reverse">
+            <Button 
+                variant="primary"
+                onClick={handlePublish}
+                disabled={!title || !story}
+            >Publish</Button>
+        </div> 
+        
+        <div>
+            <input 
+                type='text'
+                name='title'
+                placeholder='Title of your story...'
+                required
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                className='border-0 rounded-lg bg-gray-100 focus:outline-none w-full text-3xl font-serif px-5 py-3 mb-6'
+            />
 
-        <textarea 
-            type='text'
-            name='body'
-            placeholder='Tell your story...'
-            required
-            value={story}
-            onChange={(e) => setStory(e.target.value)}
-            className='border-0 rounded-lg bg-gray-100 focus:outline-none w-full text-xl leading-relaxed font-serif px-5 py-3 min-h-96 resize-none'
-        />
+            <textarea 
+                type='text'
+                name='body'
+                placeholder='Tell your story...'
+                required
+                value={story}
+                onChange={(e) => setStory(e.target.value)}
+                className='border-0 rounded-lg bg-gray-100 focus:outline-none w-full text-xl leading-relaxed font-serif px-5 py-3 min-h-96 resize-none'
+            />
+        </div>
 
-        <Button 
-            className="mt-5"
-            onClick={handlePublish}
-            disabled={!title || !story}
-        >Publish</Button>
     </div>
   )
 }
