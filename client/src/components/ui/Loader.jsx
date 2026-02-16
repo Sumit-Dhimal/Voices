c687035708b04c9039865271f355bc002d8e2f12
Sumit Dhimal <sumit.dhimal.333@gmail.com>
2026-02-16 21:35:22 +0545

    Redesigning UI

--- client/src/components/ui/Loader.jsx
+++
@@ -1,9 +0,0 @@
-const Loader = () => {
-  return (
-    <div className="flex justify-center items-center h-64">
-      <p className="text-xl font-medium animate-pulse">Loading...</p>
-    </div>
-  );
-};
-
-export default Loader;
